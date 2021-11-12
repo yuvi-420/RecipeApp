0660c5f176749085186917a3d63b19516da0c8ee
yuvi <yuvi>
2021-11-11 18:58:42 -0800

    Initial Commit

---
+++ recipeApp/recipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipeAppApp.swift
+//  recipeApp
+//
+//  Created by yuvipreet love on 11/11/21.
+//
+
+import SwiftUI
+
+@main
+struct recipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
