7e1fe4b8e307cd723652354046fd62e3907e4571
yuvi <yuvi>
2021-11-28 21:49:27 -0800

    list to scroll view and also added LazyStack!!

--- recipeApp/Views/ContentView.swift
+++ recipeApp/Views/ContentView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     
-   
-//    @ObservedObject var model = RecipeModel()
+    
+    //    @ObservedObject var model = RecipeModel()
     
     @EnvironmentObject var model: RecipeModel
     
@@ -18,36 +18,49 @@ struct ContentView: View {
         
         
         NavigationView {
-            List(model.recipes) {
-                
-                r in
-               
-                NavigationLink(destination: RecipeDetalisView(recipe:r), label: {
-                    HStack(spacing: 20){
-                        
-                        Image(r.image)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width:50, height:50, alignment:.center)
-                            .clipped()
-                            .cornerRadius(5)
-                        Text(r.name)
+            VStack(alignment: .leading, spacing: 10){
+                Text("All Recipes")
+                    .padding(.leading)
+                    .padding(.top,40)
+                    .font(.largeTitle)
+                ScrollView{
+                    LazyVStack(alignment: .leading, spacing: 10){
+                        ForEach(model.recipes) {
+                            
+                            r in
+                            
+                            NavigationLink(destination: RecipeDetalisView(recipe:r), label: {
+                                HStack(spacing: 20){
+                                    
+                                    Image(r.image)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width:50, height:50, alignment:.center)
+                                        .clipped()
+                                        .cornerRadius(5)
+                                    Text(r.name)
+                                        .foregroundColor(.black)
+                                }
+                            })
+                            
+                        }
                     }
-                })
+                }
                 
+                //            .navigationBarTitle("All Recpies")
+                .navigationBarHidden(true)
             }
-            .navigationBarTitle("All Recpies")
-        
-        }
-
+            
         }
     }
-  
+   
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-.previewInterfaceOrientation(.portraitUpsideDown)
+            .environmentObject(RecipeModel())
+        //.previewInterfaceOrientation(.portraitUpsideDown)
     }
 }
 
